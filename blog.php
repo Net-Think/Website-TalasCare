3122a033105b2d158b8c536feec63bcd491847f3
aliyea <aliyahraihan91@gmail.com>
2023-11-04 21:57:53 +0700

    beranda

--- blog.php
+++ blog.php
@@ -164,11 +164,10 @@
                     ><i class="ti-time mr-1"></i> 28th January</span
                   >
                 </div>
-
+                
                 <h3 class="mt-3 mb-3">
                   <a href="blog-detail.php"
-                    >Manfaat Tanaman Talas bagi Kehidupan Manusia</a
-                  >
+                    >Manfaat Tanaman Talas bagi Kehidupan Manusia</a>
                 </h3>
                 <p class="mb-4">
                   Ada banyak sekali jenis tanaman yang ada di dunia ini. Dari
@@ -384,7 +383,6 @@
 
     <!-- ========================= footer start ========================= -->
     <footer id="footer" class="footer">
-      <br /><br />
       <!-- <div class="footer-shape">
         <img
           src="assets/img/footer/asd.png"
@@ -406,64 +404,51 @@
                     />
                     <img src="assets/img/logo/logo.png" alt="" class="logo-1" />
                   </a>
+                  <div class="accordion-body">
+                  <p>
+                  Sumber informasi dan komunitas peduli tanaman talas di Bogor. Tempat berbagi pengalaman
+                dan belajar untuk mendapatkan wawasan baru dalam menjaga kesehatan tanaman talas. </p>
+                <p>© TalasCare 2023</p>
+            </div>
                 </div>
-                <ul class="socials">
-                  <li>
-                    <a href="#">
-                      <i class="lni lni-facebook-filled"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="lni lni-twitter-filled"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="lni lni-instagram-filled"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="lni lni-linkedin-original"></i>
-                    </a>
-                  </li>
-                </ul>
+               
               </div>
             </div>
 
             <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
               <div class="footer-widget">
-                <h3>Lorem</h3>
-                <ul class="links">
-                  <li><a href="#">Lorem Ipsum</a></li>
-                  <li><a href="#">Lorem Ipsum</a></li>
-                  <li><a href="#">Lorem Ipsum</a></li>
-                  <li><a href="#">Lorem Ipsum</a></li>
-                </ul>
+                <h3>Alamat</h3>
+                <p>Jln Kumbang No. 14, RT.02/RW.06, Babakan, Kec. Bogor Tengah, Kota Bogor, Jawa Barat 16128</p>
               </div>
             </div>
 
             <div class="col-xl-3 col-lg-2 col-md-6 col-sm-6">
               <div class="footer-widget">
-                <h3>Lorem</h3>
-                <ul class="links">
-                  <li><a href="#">Lorem Ipsum</a></li>
-                  <li><a href="#">Lorem Ipsum</a></li>
-                  <li><a href="#">Lorem Ipsum</a></li>
-                  <li><a href="#">Lorem Ipsum</a></li>
-                </ul>
+                <h3>Kontak</h3>
+                <p>Email: sv@apps.ipb.ac.id<p>
+                <p>No Telp: (0251) 8376845<p>
               </div>
             </div>
 
             <div class="col-xl-2 col-lg-3 col-md-6 col-sm-6">
               <div class="footer-widget">
-                <h3>Lorem</h3>
-                <ul class="links">
-                  <li><a href="#">Lorem Ipsum </a></li>
-                  <li><a href="#">Lorem Ipsum </a></li>
-                  <li><a href="#">Lorem Ipsum</a></li>
-                  <li><a href="#">Lorem Ipsum</a></li>
+                <h3></h3>
+                <ul class="socials">
+                  <li>
+                    <a href="facebook.com">
+                      <i class="lni lni-facebook-filled"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="twitter.com">
+                      <i class="lni lni-twitter-filled"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="instagram.com">
+                      <i class="lni lni-instagram-filled"></i>
+                    </a>
+                  </li>
                 </ul>
               </div>
             </div>
@@ -473,7 +458,9 @@
         <div class="copy-right-wrapper">
           <p>
             Designed and Developed by
-            <a href="#" rel="nofollow" target="_blank">Net Think</a>
+            <a href="#" rel="nofollow" target="_blank"
+              >Net Think</a
+            >
           </p>
         </div>
       </div>
--- index.php
+++ index.php
@@ -167,10 +167,6 @@
               <h1 class="mb-25 wow fadeInUp" data-wow-delay=".2s">
                 Visi Misi Talas Care
               </h1>
-              <p class="wow fadeInUp" data-wow-delay=".4s">
-                Lorem ipsum dolor samet consetetur sadipscing elitr, serewd diam
-                nonumy eirmod tempor invidunt ut labore.
-              </p>
             </div>
           </div>
         </div>
@@ -179,42 +175,30 @@
           <div class="col-lg-4 col-md-8 col-sm-10">
             <div class="single-feature">
               <div class="icon color-1">
-                <i class="lni lni-target"></i>
+              <i class="lni lni-heart"></i>
               </div>
               <div class="content">
-                <h3>Lorem ipsum</h3>
-                <p>
-                  Lorem ipsum dolor samet consetet sadip scing elitr serewd diam
-                  nonumy eirmod tempor invidunt ut labore.
-                </p>
+                <h3>Solusi Kesehatan Tanaman Talas Yang Terpercaya</h3>
               </div>
             </div>
           </div>
           <div class="col-lg-4 col-md-8 col-sm-10">
             <div class="single-feature">
               <div class="icon color-2">
-                <i class="lni lni-rocket"></i>
+              <i class="lni lni-eye"></i>
               </div>
               <div class="content">
-                <h3>Lorem ipsum</h3>
-                <p>
-                  Lorem ipsum dolor samet consetet sadip scing elitr serewd diam
-                  nonumy eirmod tempor invidunt ut labore.
-                </p>
+                <h3>Deteksi Hama dan Penyakit Tanaman Talas dengan AI</h3>
               </div>
             </div>
           </div>
           <div class="col-lg-4 col-md-8 col-sm-10">
             <div class="single-feature">
               <div class="icon color-3">
-                <i class="lni lni-eye"></i>
+                <i class="lni lni-grow"></i>
               </div>
               <div class="content">
-                <h3>Lorem ipsum</h3>
-                <p>
-                  Lorem ipsum dolor samet consetet sadip scing elitr serewd diam
-                  nonumy eirmod tempor invidunt ut labore.
-                </p>
+                <h3>Dukung Ketahanan Pangan Indonesia</h3>
               </div>
             </div>
           </div>
@@ -274,114 +258,79 @@
     <!-- ========================= feature-section end ========================= -->
 
     <!-- ========================= tracking-section start ========================= -->
-    <section id="tracking" class="tracking-section pt-150">
-      <div class="tracking-shape">
-        <img
-          src="assets/img/footer/asd.png"
-          alt=""
-          class="shape shape-1"
-        />
-      </div>
-      <div class="container">
-        <div class="row align-items-center">
-          <div class="col-lg-6 col-xl-7">
-            <div class="tracking-image">
-              <img src="assets/img/plan/zxc.png" width="100%" alt="" />
-            </div>
-          </div>
-          <div class="col-lg-6 col-xl-5">
-            <div class="tracking-content">
-              <div class="section-title mb-50">
+    <section class="search-section pt-150">
+      <div class="container pt-100">
+        <div class="row align-items-left">
+
+            <div class="search-content">
+              <div class="section-title text-left mb-50">
                 <h1 class="mb-40 wow fadeInUp" data-wow-delay=".2s">
-                  Ciri-ciri Umum Talas Anda Terjangkit Hama dan Penyakit
+                Talas Terserang Hama & Penyakit? Perhatikan Ciri-cirinya!
                 </h1>
-                <p class="wow fadeInUp" data-wow-delay=".4s">
-                  Berikut adalah beberapa ciri-ciri umum talas yang terjangkit hama dan penyakit:
+
+                <div class="container">
+          <div class="row">
+            <div class="col-xl-5 col-lg-5">
+              <div class="blog-item">
+                <img src="assets/images/talas-lubang.png"
+                alt=""
+                class="img-fluid rounded float-left"/>
+              </div>
+            </div>
+
+            <div class="text-left col-xl-6">
+                <h3>Daun Tanaman Berlubang</h3>
+                <p>
+                  Biasanya, hama yang sudah diketahui sering menyerang tanaman talas dengan ciri-ciri
+                  di antaranya: <br>• Ngengat Elang Anggur, <br>• Belalang Bertanduk Pendek, <br>• Wereng Pucuk Teh,<br>• dan Ulat Grayak.
                 </p>
-              </div>
-              <div class="single-plan">
-                <div class="icon-style color-1">
-                  <i class="lni lni-leaf"></i>
-                </div>
-                <div class="content">
-                  <h3>Daun yang Menguning atau Menggugurkan</h3>
-                  <p>
-                    Daun talas yang menguning atau gugur secara berlebihan bisa menjadi tanda adanya masalah.
-                  </p>
-                </div>
-              </div>
-              <div class="single-plan">
-                <div class="icon-style color-2">
-                  <i class="lni lni-tree"></i>
-                </div>
-                <div class="content">
-                  <h3>Bintik-bintik atau Noda pada Daun</h3>
-                  <p>
-                    Bintik-bintik hitam, cokelat, atau putih pada daun talas adalah indikasi serangan penyakit seperti jamur atau bakteri.
-                  </p>
-                </div>
-              </div>
-              <div class="single-plan">
-                <div class="icon-style color-3">
-                  <i class="lni  lni-sprout"></i>
-                </div>
-                <div class="content">
-                  <h3>Daun yang Lecet atau Berlubang</h3>
-                  <p>
-                    Daun talas yang memiliki bekas gigitan, lubang, atau kerusakan fisik lainnya, hal ini mungkin disebabkan oleh serangan hama seperti ulat atau kutu.
-                  </p>
-                </div>
               </div>
             </div>
           </div>
         </div>
+        <div class="row justify-content-center">
+			<div class="col-lg-6 col-md-12 col-sm-12">
+		<a
+                  href="#"
+                  class="map-btn btn-hover wow fadeInUp"
+                  data-wow-delay=".6s"
+                  >Ayo Ketahui Lebih Banyak!</a
+                >
+				</div></div>
       </div>
+      </div>
+    </div>
+  </div>
+</div>
+          </div>
     </section>
+	
     <!-- ========================= tracking-section end ========================= -->
 
     <!-- ========================= search-section start ========================= -->
     <section class="search-section pt-150">
       <div class="container">
         <div class="row align-items-center">
-          <div class="col-xl-6 col-lg-6 order-last order-lg-first">
             <div class="search-content">
-              <div class="section-title mb-40">
+              <div class="section-title text-center mb-40">
                 <h1 class="mb-40 wow fadeInUp" data-wow-delay=".2s">
-                   Cara Mencegah Tanaman Talas Agar Tidak Terjangkit Hama & Penyakit
+                   3 Fitur Andalan TalasCare
                 </h1>
-                <p class="wow fadeInUp" data-wow-delay=".4s">
-                  Berikut adalah beberapa cara mencegah tanaman talas agar tidak hama dan penyakit
-                </p>
               </div>
               <div class="app-info">
                 <div class="single-info">
-					<div class="icon-style color-1" style="padding:15px">
-						<h2><i class="lni lni-checkmark"></i></h2> 
-					  </div>
-                  <h4>Menjaga kebersihan tanaman</h4>
+                <img src="assets/image/beranda/forum.png">
+                  <h4>Forum Diskusi</h4>
                 </div>
                 <div class="single-info">
-                  <div class="icon-style color-2" style="padding:15px">
-					<h2><i class="lni lni-protection"></i></h2> 
-                  </div>
-                  <h4>Menghindari kelembaban berlebih</h4>
+                <img src="assets/image/beranda/talasdetection.png">
+                  <h4>TalasDetection</h4>
                 </div>
                 <div class="single-info">
-					<div class="icon-style color-3" style="padding:15px">
-						<h2><i class="lni lni-layers"></i></h2> 
-					  </div>
-                  <h4>Menjaga jarak antara tanaman</h4>
+					<img src="assets/image/beranda/talasai.png">
+                  <h4>TalasAI</h4>
                 </div>
               </div>
-              <p>
-                Menerapkan praktik-praktik ini dapat membantu menjaga tanaman talas Anda tetap sehat dan produktif, sambil mengurangi risiko serangan hama dan penyakit. Penting untuk selalu memantau dan merawat tanaman secara berkala untuk memastikan kesehatannya.
-              </p>
-            </div>
-          </div>
-
-          <div class="col-xl-6 col-lg-6">
-            <div class="search-img text-lg-right img-solusi">
-              <img src="assets/img/plan/cvb.png" width="100%" alt="" />
             </div>
           </div>
         </div>
@@ -409,9 +358,7 @@
           </h2>
           <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
             <div class="accordion-body">
-              <p>Diam orci gravida convallis at enim risus viverra. Hac mi tristique in aliquet tincidunt nam lectus
-                nec. Placerat interdum auctor facilisi massa laoreet hendrerit posuere a. Tristique ultricies consectetu
-                at.</p>
+              <p>Beberapa ciri umum talas terkena hama adalah daun berlubang-lubang, layu, menguning, atau kering, dan umbi membusuk.</p>
             </div>
           </div>
         </div>
@@ -420,15 +367,14 @@
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
               data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false"
               aria-controls="panelsStayOpen-collapseTwo">
-              Apa ciri - ciri tanaman talas terkena hama?
+              Hama apa yang sering menyerang tanaman talas?
             </button>
           </h2>
           <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse"
             aria-labelledby="panelsStayOpen-headingTwo">
             <div class="accordion-body">
-              <p>Diam orci gravida convallis at enim risus viverra. Hac mi tristique in aliquet tincidunt nam lectus
-                nec. Placerat interdum auctor facilisi massa laoreet hendrerit posuere a. Tristique ultricies consectetu
-                at.</p>
+              <p>
+Hama yang sering menyerang tanaman talas adalah ulat daun, belalang, kutu daun, penggerek batang, tungau merah, dan ulat grayak. Hama-hama ini dapat menyerang bagian daun, batang, maupun umbi talas.</p>
             </div>
           </div>
         </div>
@@ -437,15 +383,14 @@
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
               data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false"
               aria-controls="panelsStayOpen-collapseThree">
-              Apa ciri - ciri tanaman talas terkena hama?
+              Penyakit apa yang sering menyerang tanaman talas?
             </button>
           </h2>
           <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse"
             aria-labelledby="panelsStayOpen-headingThree">
             <div class="accordion-body">
-              <p>Diam orci gravida convallis at enim risus viverra. Hac mi tristique in aliquet tincidunt nam lectus
-                nec. Placerat interdum auctor facilisi massa laoreet hendrerit posuere a. Tristique ultricies consectetu
-                at.</p>
+              <p>
+Penyakit yang sering menyerang tanaman talas adalah busuk umbi, hawar daun, dan layu bakteri. Penyakit-penyakit ini dapat menyebabkan kematian tanaman talas.</p>
             </div>
           </div>
         </div>
@@ -454,15 +399,13 @@
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
               data-bs-target="#panelsStayOpen-collapseFour" aria-expanded="false"
               aria-controls="panelsStayOpen-collapseFour">
-              Apa ciri - ciri tanaman talas terkena hama?
+              Bagaimana cara mengendalikan hama belalang?
             </button>
           </h2>
           <div id="panelsStayOpen-collapseFour" class="accordion-collapse collapse"
             aria-labelledby="panelsStayOpen-headingFour">
             <div class="accordion-body">
-              <p>Diam orci gravida convallis at enim risus viverra. Hac mi tristique in aliquet tincidunt nam lectus
-                nec. Placerat interdum auctor facilisi massa laoreet hendrerit posuere a. Tristique ultricies consectetu
-                at.</p>
+              <p>Hama belalang dapat dikendalikan dengan cara pengendalian fisik, hayati, atau kimia. Pengendalian fisik dilakukan dengan mengumpulkan dan memusnahkan belalang secara manual. Pengendalian hayati dilakukan dengan memanfaatkan musuh alami belalang, seperti burung, katak, dan capung. Pengendalian kimia dilakukan dengan menggunakan pestisida yang sesuai.</p>
             </div>
           </div>
         </div>
@@ -471,32 +414,13 @@
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
               data-bs-target="#panelsStayOpen-collapseFive" aria-expanded="false"
               aria-controls="panelsStayOpen-collapseFive">
-              Apa ciri - ciri tanaman talas terkena hama?
+              Berapa rasio pupuk urea yang baik untuk tanaman talas?
             </button>
           </h2>
           <div id="panelsStayOpen-collapseFive" class="accordion-collapse collapse"
             aria-labelledby="panelsStayOpen-headingFive">
             <div class="accordion-body">
-              <p>Diam orci gravida convallis at enim risus viverra. Hac mi tristique in aliquet tincidunt nam lectus
-                nec. Placerat interdum auctor facilisi massa laoreet hendrerit posuere a. Tristique ultricies consectetu
-                at.</p>
-            </div>
-          </div>
-        </div>
-        <div class="accordion-item mt-3" data-aos="fade-up" data-aos-easing="ease-in-sine" data-aos-delay="500">
-          <h2 class="accordion-header" id="panelsStayOpen-headingSix">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-              data-bs-target="#panelsStayOpen-collapseSix" aria-expanded="false"
-              aria-controls="panelsStayOpen-collapseSix">
-              Apa ciri - ciri tanaman talas terkena hama?
-            </button>
-          </h2>
-          <div id="panelsStayOpen-collapseSix" class="accordion-collapse collapse"
-            aria-labelledby="panelsStayOpen-headingSix">
-            <div class="accordion-body">
-              <p>Diam orci gravida convallis at enim risus viverra. Hac mi tristique in aliquet tincidunt nam lectus
-                nec. Placerat interdum auctor facilisi massa laoreet hendrerit posuere a. Tristique ultricies consectetu
-                at.</p>
+              <p>Rasio pupuk urea yang baik untuk tanaman talas adalah 100-150 kg urea/ha. Rasio ini dapat disesuaikan dengan kondisi lahan dan jenis talas yang dibudidayakan.</p>
             </div>
           </div>
         </div>
@@ -530,64 +454,51 @@
                     />
                     <img src="assets/img/logo/logo.png" alt="" class="logo-1" />
                   </a>
+                  <div class="accordion-body">
+                  <p>
+                  Sumber informasi dan komunitas peduli tanaman talas di Bogor. Tempat berbagi pengalaman
+                dan belajar untuk mendapatkan wawasan baru dalam menjaga kesehatan tanaman talas. </p>
+                <p>© TalasCare 2023</p>
+            </div>
                 </div>
-                <ul class="socials">
-                  <li>
-                    <a href="#">
-                      <i class="lni lni-facebook-filled"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="lni lni-twitter-filled"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="lni lni-instagram-filled"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="lni lni-linkedin-original"></i>
-                    </a>
-                  </li>
-                </ul>
+               
               </div>
             </div>
 
             <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
               <div class="footer-widget">
-                <h3>Lorem</h3>
-                <ul class="links">
-                  <li><a href="#">Lorem Ipsum</a></li>
-                  <li><a href="#">Lorem Ipsum</a></li>
-                  <li><a href="#">Lorem Ipsum</a></li>
-                  <li><a href="#">Lorem Ipsum</a></li>
-                </ul>
+                <h3>Alamat</h3>
+                <p>Jln Kumbang No. 14, RT.02/RW.06, Babakan, Kec. Bogor Tengah, Kota Bogor, Jawa Barat 16128</p>
               </div>
             </div>
 
             <div class="col-xl-3 col-lg-2 col-md-6 col-sm-6">
               <div class="footer-widget">
-                <h3>Lorem</h3>
-                <ul class="links">
-                  <li><a href="#">Lorem Ipsum</a></li>
-                  <li><a href="#">Lorem Ipsum</a></li>
-                  <li><a href="#">Lorem Ipsum</a></li>
-                  <li><a href="#">Lorem Ipsum</a></li>
-                </ul>
+                <h3>Kontak</h3>
+                <p>Email: sv@apps.ipb.ac.id<p>
+                <p>No Telp: (0251) 8376845<p>
               </div>
             </div>
 
             <div class="col-xl-2 col-lg-3 col-md-6 col-sm-6">
               <div class="footer-widget">
-                <h3>Lorem</h3>
-                <ul class="links">
-                  <li><a href="#">Lorem Ipsum </a></li>
-                  <li><a href="#">Lorem Ipsum </a></li>
-                  <li><a href="#">Lorem Ipsum</a></li>
-                  <li><a href="#">Lorem Ipsum</a></li>
+                <h3></h3>
+                <ul class="socials">
+                  <li>
+                    <a href="facebook.com">
+                      <i class="lni lni-facebook-filled"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="twitter.com">
+                      <i class="lni lni-twitter-filled"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="instagram.com">
+                      <i class="lni lni-instagram-filled"></i>
+                    </a>
+                  </li>
                 </ul>
               </div>
             </div>
