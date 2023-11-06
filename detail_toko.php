495b854d73d049a82aabc9ccb29eb8b7a6f6f3c5
zakyahmadfaisal <zakyahmad@apps.ipb.ac.id>
2023-11-06 17:31:16 +0700

    Membuat tampilan TalasMap dan Detail Toko

---
+++ detail_toko.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html class="no-js" lang="">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>AppGrids - Bootstrap 5 App Landing Page Template</title>
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      rel="shortcut icon"
+      type="image/x-icon"
+      href="assets/img/favicon.svg"
+    />
+    <!-- ========================= CSS here ========================= -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+      crossorigin="anonymous"
+    ></script>
+    <link rel="stylesheet" href="assets/plugins/themify/css/themify-icons.css" />
+    <link rel="stylesheet" href="assets/css/LineIcons.2.0.css" />
+    <link rel="stylesheet" href="assets/css/animate.css" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+  </head>
+  <body>
+    <!-- ========================= preloader start ========================= -->
+    <div class="preloader">
+      <div class="loader">
+        <div class="ytp-spinner">
+          <div class="ytp-spinner-container">
+            <div class="ytp-spinner-rotator">
+              <div class="ytp-spinner-left">
+                <div class="ytp-spinner-circle"></div>
+              </div>
+              <div class="ytp-spinner-right">
+                <div class="ytp-spinner-circle"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- preloader end -->
+
+    <!-- ========================= header start ========================= -->
+    <header class="header">
+      <div class="navbar-area">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-12">
+              <nav class="navbar navbar-expand-lg">
+                <a class="navbar-brand" href="index.php">
+                  <img src="assets/img/logo/logo.png" alt="Logo" />
+                </a>
+                <button
+                  class="navbar-toggler"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span class="toggler-icon"></span>
+                  <span class="toggler-icon"></span>
+                  <span class="toggler-icon"></span>
+                </button>
+
+                <div
+                  class="collapse navbar-collapse sub-menu-bar justify-content-end"
+                  id="navbarSupportedContent"
+                >
+                  <ul id="nav" class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                      <a href="index.php">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="page-scroll" href="forum.php">Forum</a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="blog.php">Blog</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="page-scroll" href="#download">Talas</a>
+                      <div class="dropdown-content">
+                        <a href="TalasInfo.php">TalasInfo</a>
+                        <a href="TalasAI.php">TalasAI</a>
+                        <a href="TalasMaps.php">TalasMaps</a>
+                        <a href="TalasDetection.php">TalasDetection</a>
+                      </div>
+                    </li>
+                    <li class="nav-item2">
+                      <div class="border-first-button">
+                        <a href="#contact">Sign In</a>
+                      </div>
+                    </li>
+                    <!-- <li class="nav-item3">
+                      <div class="border-first-button">
+                        <a href="#contact">Sign Up</a>
+                      </div>
+                    </li> -->
+                  </ul>
+                </div>
+                <!-- navbar collapse -->
+              </nav>
+              <!-- navbar -->
+            </div>
+          </div>
+          <!-- row -->
+        </div>
+        <!-- container -->
+      </div>
+      <!-- navbar area -->
+    </header>
+    <!-- ========================= header end ========================= -->
+
+    <!-- ========================= hero-section start ========================= -->
+    <section class="page-title bg-1">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="block text-center">
+                <span class="text-white">Detail Toko</span>
+                <h1 class="text-capitalize mb-4 text-lg">TalasMap</h1>
+                <ul class="list-inline">
+                  <li class="list-inline-item">
+                    <a href="index.php" class="text-white-50">Home</a>
+                  </li>
+                  <li class="list-inline-item">
+                    <span class="text-white">/</span>
+                  </li>
+                  <li class="list-inline-item">
+                    <a href="#" class="text-white">TalasMap</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="section blog-wrap bg-gray">
+          <div class="container">
+              <div class="row">
+                  <div class="col-lg-8">
+                      <div class="row">
+          <div class="col-lg-12 mb-5">
+              <div class="single-blog-item">
+                  <h2 class="mt-3 mb-4">Tani Jaya</h2><br/>
+                  <div class="blog-item-content bg-white p-5">
+                      <img src="images/Bintang/Gambar toko.jpg" alt="" class="img-fluid rounded">
+                      <img src="images/Bintang/Bintang_5.png" alt="" class="mt-4"/> 
+                      <h3 class="mt-4">Deskripsi</h3>
+                      <p class="mt-2">Menjual berbagai keperluan pertanian, benih, pestisida, dan bunga artificial</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+                  </div>
+                  <div class="col-lg-4">
+                      <div class="sidebar-wrap">
+                        
+      
+          <div class="sidebar-widget latest-post card border-0 p-4 mb-3 mt-105">
+            
+              <ul class="unstyle">
+                <li><h4>Kontak</h4></li>
+                <li><b class="propertyname">Alamat:</b> Jl. Dewi Sartika No.56, Pabaton, Kecamatan Bogor Tengah, Kota Bogor, Jawa Barat 16121</li>
+                <li><b class="propertyname">Waktu:</b> Hari: Senin-Sabtu Pukul: 08.00-15.30</li>
+                <li><b class="propertyname">Website:</b> <a href="business.site">business.site</a></li>
+                <li><b class="propertyname">Telepon:</b> 081511771456</li>      
+              <div class="media border-bottom py-3">
+            
+          </div>
+      
+         
+      </div>
+                  </div>   
+              </div>
+          </div>
+      </section>
+    <!-- ========================= hero-section end ========================= -->
+
+    <!-- ========================= footer start ========================= -->
+    <footer id="footer" class="footer">
+      <!-- <div class="footer-shape">
+        <img
+          src="assets/img/footer/asd.png"
+          alt=""
+          class="shape shape-1"
+        />
+      </div> -->
+      <div class="container">
+        <div class="widget-wrapper">
+          <div class="row">
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+              <div class="footer-widget">
+                <div class="logo">
+                  <a href="index.php">
+                    <img
+                      src="assets/img/logo/logo2.png"
+                      alt=""
+                      class="logo-2"
+                    />
+                    <img src="assets/img/logo/logo.png" alt="" class="logo-1" />
+                  </a>
+                  <div class="accordion-body">
+                  <p>
+                  Sumber informasi dan komunitas peduli tanaman talas di Bogor. Tempat berbagi pengalaman
+                dan belajar untuk mendapatkan wawasan baru dalam menjaga kesehatan tanaman talas. </p>
+                <p>© TalasCare 2023</p>
+            </div>
+                </div>
+               
+              </div>
+            </div>
+
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+              <div class="footer-widget">
+                <h3>Alamat</h3>
+                <p>Jln Kumbang No. 14, RT.02/RW.06, Babakan, Kec. Bogor Tengah, Kota Bogor, Jawa Barat 16128</p>
+              </div>
+            </div>
+
+            <div class="col-xl-3 col-lg-2 col-md-6 col-sm-6">
+              <div class="footer-widget">
+                <h3>Kontak</h3>
+                <p>Email: sv@apps.ipb.ac.id<p>
+                <p>No Telp: (0251) 8376845<p>
+              </div>
+            </div>
+
+            <div class="col-xl-2 col-lg-3 col-md-6 col-sm-6">
+              <div class="footer-widget">
+                <h3></h3>
+                <ul class="socials">
+                  <li>
+                    <a href="facebook.com">
+                      <i class="lni lni-facebook-filled"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="twitter.com">
+                      <i class="lni lni-twitter-filled"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="instagram.com">
+                      <i class="lni lni-instagram-filled"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="copy-right-wrapper">
+          <p>
+            Designed and Developed by
+            <a href="#" rel="nofollow" target="_blank"
+              >Net Think</a
+            >
+          </p>
+        </div>
+      </div>
+    </footer>
+    <!-- ========================= footer end ========================= -->
+
+    <!-- ========================= scroll-top ========================= -->
+    <a href="#" class="scroll-top btn-hover">
+      <i class="lni lni-chevron-up"></i>
+    </a>
+
+    <!-- ========================= JS here ========================= -->
+    <script src="assets/js/bootstrap.5.0.0.alpha-2-min.js"></script>
+    <script src="assets/js/count-up.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script
+      src="https://code.jquery.com/jquery-3.7.1.js"
+      integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
